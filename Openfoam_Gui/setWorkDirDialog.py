d9fb5bf4687f33edc7d28445692210f1df4d1efc
Erik0807 <erik.grosshaupt@students.fh-wels.at>
2025-01-18 14:10:20 +0100

    Klasse angelegt

---
+++ Openfoam_Gui/setWorkDirDialog.py
@@ -0,0 +1,35 @@
+# Einlesen benötigte Bibliotheken
+#================================
+from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QDialog, QHBoxLayout,
+                               QVBoxLayout, QLabel, QPushButton, QLineEdit)
+from PySide6.QtGui import QAction, QKeySequence
+from PySide6.QtCore import Slot
+from ofDialog import ofDialog
+
+
+#===================================================================================
+#                               KLASSE - setWorkDirDialog                          #
+#===================================================================================
+
+class setWorkDirDialog(ofDialog):
+    # Konstruktor
+    #============
+    def __init__(self):
+        # Mutterklassenkonstruktor
+        super().__init__()
+
+        # Titel des Fensters
+        self.setWindowTitle("Set Working Directory")
+
+        # Aufforderung an User
+        self.label = QLabel("Bitte geben Sie den Pfad zu Ihrem case ein:")
+        self.layout().insertWidget(0, self.label)
+
+        # Eingabefeld
+        self.input_field = QLineEdit()
+        self.input_field.setPlaceholderText("Pfad/zu/meinem/case")
+        self.layout().insertWidget(1, self.input_field)
+
+        # Buttonverbindungen
+        self.ok_button.clicked.connect(self.getInput)
+        self.close_button.clicked.connect(self.reject)
