d806472aeeb057b6139bc5d531256938d8e62bdd
Erik0807 <erik.grosshaupt@students.fh-wels.at>
2025-01-21 08:25:05 +0100

    Box für pFinal hinzu

--- Openfoam_Gui/fvSolutionDialog.py
+++ Openfoam_Gui/fvSolutionDialog.py
@@ -55,6 +55,12 @@ class fvSolutionDialog(ofDialog):
         self.input_tolerance.setPlaceholderText("1e-06")
         #----------------------------------------------------------------------------------
 
+        # relTol
+        #-------
+        self.label_relTol = QLabel("relTol")
+        self.input_relTol = QLineEdit()
+        self.input_relTol.setPlaceholderText("0.05")
+        #----------------------------------------------------------------------------------
 
         # Layout für Gruppierung um p
         #----------------------------
@@ -65,17 +71,46 @@ class fvSolutionDialog(ofDialog):
 
         layout_p.addWidget(self.label_preconditioner)
         layout_p.addWidget(self.dropdown_preconditioner)
-        
+
         layout_p.addWidget(self.label_tolerance)
         layout_p.addWidget(self.input_tolerance)
 
+        layout_p.addWidget(self.label_relTol)
+        layout_p.addWidget(self.input_relTol)
+
         # Anlegen der p-Box
         #------------------
-        p_box = QGroupBox("p")
-        p_box.setLayout(layout_p)
+        self.p_box = QGroupBox("p")
+        self.p_box.setLayout(layout_p)
+        #==================================================================================
 
-        # Layout von p zu Gesamtlayout
-        self.layout().insertWidget(1, p_box)
+        # Erzeugen der Dropdown-Menüs für die Dictionary-Einträge von pFinal-Solvern
+        #===========================================================================
+        # relTol
+        #-------
+        self.label_relTol_pFin = QLabel("relTol")
+        self.input_relTol_pFin = QLineEdit()
+        self.input_relTol_pFin.setPlaceholderText("0.05")
+        #----------------------------------------------------------------------------------
+
+        # Layout für Gruppierung um pFinal
+        #---------------------------------
+        layout_pFinal = QVBoxLayout()
+
+        layout_pFinal.addWidget(self.label_relTol_pFin)
+        layout_pFinal.addWidget(self.input_relTol_pFin)
+
+        # Anlegen der pFinal-Box
+        #-----------------------
+        self.pFinal_box = QGroupBox("pFinal")
+        self.pFinal_box.setLayout(layout_pFinal)
+
+
+
+
+        # Layout der Gruppen zu Gesamtlayout
+        self.layout().insertWidget(1, self.p_box)
+        self.layout().insertWidget(2, self.pFinal_box)
 
         # Buttonverbindungen
         self.ok_button.clicked.connect(self.writeDic)
