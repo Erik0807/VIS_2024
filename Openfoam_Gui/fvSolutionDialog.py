4e544ec1a4d1352035f76c4a3398d1f54525d263
Erik0807 <erik.grosshaupt@students.fh-wels.at>
2025-01-21 08:49:55 +0100

    Einträge von solver: U fertig

--- Openfoam_Gui/fvSolutionDialog.py
+++ Openfoam_Gui/fvSolutionDialog.py
@@ -104,13 +104,64 @@ class fvSolutionDialog(ofDialog):
         #-----------------------
         self.pFinal_box = QGroupBox("pFinal")
         self.pFinal_box.setLayout(layout_pFinal)
+        #==================================================================================
 
+        # Erzeugen der Dropdown-Menüs für die Dictionary-Einträge von U-Solvern
+        #======================================================================
+        # solver
+        #-------
+        self.label_solver_U = QLabel("solver")
+        self.dropdown_solver_U = QComboBox()
+        self.dropdown_solver_U.addItems(["PCG", "PBiCGStab"])
+        #----------------------------------------------------------------------------------
 
+        # preconditioner
+        #---------------
+        self.label_preconditioner_U = QLabel("preconditioner")
+        self.dropdown_preconditioner_U = QComboBox()
+        self.dropdown_preconditioner_U.addItems(["DIC", "DILU"])
+        #----------------------------------------------------------------------------------
 
+        # tolerance
+        #----------
+        self.label_tolerance_U = QLabel("tolerance")
+        self.input_tolerance_U = QLineEdit()
+        self.input_tolerance_U.setPlaceholderText("1e-06")
+        #----------------------------------------------------------------------------------
+
+        # relTol
+        #-------
+        self.label_relTol_U = QLabel("relTol")
+        self.input_relTol_U = QLineEdit()
+        self.input_relTol_U.setPlaceholderText("0.05")
+        #----------------------------------------------------------------------------------
+
+        # Layout für Gruppierung um U
+        #----------------------------
+        layout_U = QVBoxLayout()
+
+        layout_U.addWidget(self.label_solver_U)
+        layout_U.addWidget(self.dropdown_solver_U)
+
+        layout_U.addWidget(self.label_preconditioner_U)
+        layout_U.addWidget(self.dropdown_preconditioner_U)
+
+        layout_U.addWidget(self.label_tolerance_U)
+        layout_U.addWidget(self.input_tolerance_U)
+
+        layout_U.addWidget(self.label_relTol_U)
+        layout_U.addWidget(self.input_relTol_U)
+
+        # Anlegen der p-Box
+        #------------------
+        self.U_box = QGroupBox("U")
+        self.U_box.setLayout(layout_U)
+        #==================================================================================
 
         # Layout der Gruppen zu Gesamtlayout
         self.layout().insertWidget(1, self.p_box)
         self.layout().insertWidget(2, self.pFinal_box)
+        self.layout().insertWidget(3, self.U_box)
 
         # Buttonverbindungen
         self.ok_button.clicked.connect(self.writeDic)
